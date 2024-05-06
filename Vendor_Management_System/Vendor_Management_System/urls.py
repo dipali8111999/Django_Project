7d5b89f1937817abf5b2259cb4e8203bb232be6c
Dipali <dipalichavai12@gmail.com>
2024-05-06 09:07:41 +0530

    'Vendor_management_system'

--- Vendor_Management_System/Vendor_Management_System/urls.py
+++ Vendor_Management_System/Vendor_Management_System/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from api.views import VendorViewSet, PurchaseOrderViewSet
+
+router = DefaultRouter()
+router.register(r'vendors', VendorViewSet)
+router.register(r'purchase_orders', PurchaseOrderViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-  
+    path('api/', include(router.urls)),
 ]
